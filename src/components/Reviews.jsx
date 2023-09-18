b4dea30501f04967c2e6367d8a7314506d7fc1cf
kristinegusta <kristine.guta@maastrichtuniversity.nl>
2023-09-18 13:04:35 +0200

    use a widget for facebook reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,12 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Section from "./Section";
 import styled from "styled-components";
 import { device } from "../assets/breakpoints";
-import {
-  firstReview,
-  secondReview,
-  thirdReview,
-} from "../assets/facebookReviews";
 
 export default function Reviews() {
   return (
@@ -19,11 +14,7 @@ export default function Reviews() {
           physiotherapist.
         </p>
       </article>
-      <ReviewsContainer>
-        <Review dangerouslySetInnerHTML={{ __html: firstReview }} />
-        <Review dangerouslySetInnerHTML={{ __html: secondReview }} />
-        <Review dangerouslySetInnerHTML={{ __html: thirdReview }} />
-      </ReviewsContainer>
+      <div className="elfsight-app-624b2944-96d0-4233-a75d-6a7ba0b89829"></div>
     </Section>
   );
 }
@@ -40,32 +31,5 @@ const Title = styled.h2`
       margin-left: auto;
       margin-right: auto;
     }
-  }
-`;
-
-const ReviewsContainer = styled.section`
-  @media ${device.tablet} {
-    display: flex;
-    justify-content: space-between;
-  }
-`;
-
-const Review = styled.article`
-    position: relative;
-  width: 100%;
-  margin-bottom: 20px;
-  @media ${device.tablet} {
-    width: 32%;
-    height: 500px;
-    overflow-y: scroll;
-    border-bottom: 1px solid #dddfe2;
-    &::after {
-        width: 100%;
-        position: sticky;
-        bottom: 0;
-        display: flex;
-        background: linear-gradient(to bottom, rgba(2, 0, 36, 0) 0%, rgb(255, 255, 255) 60%);
-        content: "";
-        height: 30px;
   }
 `;
