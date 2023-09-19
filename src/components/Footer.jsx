ff9912d926af23d5b008a0e43d629be67c2c1c91
kristinegusta <kristine.guta@maastrichtuniversity.nl>
2023-09-19 21:57:40 +0200

    Add desktop footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,43 +4,47 @@ import { device } from "../assets/breakpoints";
 import Section from "./Section";
 import { FaInstagram, FaWhatsapp } from "react-icons/fa";
 import { FiMail } from "react-icons/fi";
+import Logo from "../images/elizabete.png";
 
 export default function Footer() {
   return (
     <Section name="contact">
-      <FooterContainerSmall>
-        <FooterTitle>Location</FooterTitle>
-        <p>
-          I am located in Leiden, the Netherlands, but I frequently travel to
-          various destinations within the Netherlands and Belgium.
-          <br />
-          <br />
-          Woudstraat 23 <br></br>
-          EN 6104 Leiden <br></br>
-          Netherlands
-        </p>
-      </FooterContainerSmall>
-      <FooterContainerSmall>
-        <FooterTitle>Contact</FooterTitle>
-        <p>
-          <Icon>
-            <FiMail />
-          </Icon>
-          elizabete@elizabete.com
-        </p>
-        <p>
-          <Icon>
-            <FaWhatsapp />
-          </Icon>
-          +31 22112277
-        </p>
-        <p>
-          <Icon>
-            <FaInstagram />
-          </Icon>
-          pringlelv
-        </p>
-      </FooterContainerSmall>
+      <FooterContainer>
+        <LogoContainer />
+        <FooterContainerSmall>
+          <FooterTitle>Location</FooterTitle>
+          <p>
+            I am located in Leiden, the Netherlands, but I frequently travel to
+            various destinations within the Netherlands and Belgium.
+            <br />
+            <br />
+            Woudstraat 23 <br></br>
+            EN 6104 Leiden <br></br>
+            Netherlands
+          </p>
+        </FooterContainerSmall>
+        <FooterContainerSmall>
+          <FooterTitle>Contact</FooterTitle>
+          <p>
+            <Icon>
+              <FiMail />
+            </Icon>
+            elizabete@elizabete.com
+          </p>
+          <p>
+            <Icon>
+              <FaWhatsapp />
+            </Icon>
+            +31 22112277
+          </p>
+          <p>
+            <Icon>
+              <FaInstagram />
+            </Icon>
+            pringlelv
+          </p>
+        </FooterContainerSmall>
+      </FooterContainer>
       <Seperator />
       <FooterFooter>
         2023 © E’Braun equestrian・Powered by <span>Logo</span>
@@ -49,11 +53,33 @@ export default function Footer() {
   );
 }
 
+const LogoContainer = styled.div`
+  display: none;
+  background-image: url(${Logo});
+  width: 270px;
+  background-size: contain;
+  background-position: center;
+  margin-bottom: 3rem;
+  @media ${device.tablet} {
+    display: block;
+  }
+`;
+
+const FooterContainer = styled.div`
+  @media ${device.tablet} {
+    display: flex;
+    justify-content: space-around;
+  }
+`;
+
 const FooterContainerSmall = styled.div`
   margin-bottom: 3rem;
   p {
     text-align: center;
     margin-bottom: 1rem;
+  }
+  @media ${device.tablet} {
+    width: 270px;
   }
 `;
 
